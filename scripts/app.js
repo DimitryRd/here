5bf890e3f4b2d34ab8d4a2fe2d6714ef1ff7618a
Dmitry Rudakov <dimitry.rd@gmail.com>
2019-01-15 22:42:07 +0200

    Marker added with icon

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,43 @@ const platform = new H.service.Platform({
 });
 
 const defaultLayers = platform.createDefaultLayers();
-const mapContainer = document.getElementById('ma-container');
+const mapContainer = document.getElementById('map-container');
+
+const coordinates = {
+    lat: 52.530974,
+    lng: 13.384944
+};
+
+const mapOptions = {
+    center: coordinates,
+    zoom: 14
+};
 
 const map = new H.Map(
     mapContainer,
-    defaultLayers.normal.map
+    defaultLayers.normal.map,
+    mapOptions
 );
+
+map.setBaseLayer(defaultLayers.satellite.traffic);
 
 window.addEventListener('resize', function(){
     map.getViewPort().resize();
 });
+
+const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+const iconUrl = './images/marker-gelato.svg';
+
+const iconOptions = {
+    size: new H.math.Size(26,34),
+    anchor: new H.math.Point(14,34)
+};
+
+const markerOptions = {
+    icon: new H.map.Icon(iconUrl, iconOptions)
+};
+
+const marker = new H.map.Marker(coordinates, markerOptions);
+
+map.addObject(marker);
